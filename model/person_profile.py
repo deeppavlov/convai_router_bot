f53b72db3e448365f26097b09bf683282a0ed141
litinsky <litinsky@ipavlov.ai>
2019-05-17 19:38:24 +0300

    Added linked person profiles importing

--- model/person_profile.py
+++ model/person_profile.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from mongoengine import *
 
@@ -6,6 +6,7 @@ from mongoengine import *
 class PersonProfile(Document):
     """Profile assigned to the conversation participant"""
     sentences: List[str] = ListField(StringField(), required=True)
+    link_uuid: Optional[str] = StringField(required=False)
 
     @property
     def description(self) -> str:
