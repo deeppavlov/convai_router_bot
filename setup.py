6b02dbc43f4c0ce064283b6f9c5eeeaa31d1704c
Kuznetsov Denis Pavlovich <kuznetsov.den.p@gmail.com>
2019-10-24 18:34:39 +0300

    feat (setup): fix typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='router_botga',
+    name='router_bot',
     version='0.0.1',
     include_package_data=True,
     packages=find_packages(),
